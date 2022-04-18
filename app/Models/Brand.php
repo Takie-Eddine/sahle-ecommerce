43a4d4de76e14e554dc48ea4b6a5ac2d35fb71c1
yacoubdemire <yacoubdemire@gmail.com>
2022-04-18 17:25:03 +0100

    brandCRUD

---
+++ app/Models/Brand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    use Translatable;
+
+
+    protected $with = ['translations'];
+
+    protected $fillable = ['is_active','photo'];
+
+    protected $cast =[
+        'is_active' => 'boolean',
+    ];
+
+    protected $translatedAttributes = ['name'];
+
+
+
+    public function getActive(){
+        return  $this -> is_active  == 0 ?  'غير مفعل'   : 'مفعل' ;
+    }
+
+
+    public function  getPhotoAttribute($val){
+        return ($val !== null) ? asset('assets/images/brands/' . $val) : "";
+    }
+
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'brands' => [
+            'driver' => 'local',
+            'root' => public_path('assets/images/brands'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
