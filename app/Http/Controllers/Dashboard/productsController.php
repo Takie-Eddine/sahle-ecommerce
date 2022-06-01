87565f1543c55500d07164e3985a62057b479ef7
yacoubdemire <yacoubdemire@gmail.com>
2022-06-01 16:02:09 +0100

    product attributes and options

--- app/Http/Controllers/Dashboard/productsController.php
+++ app/Http/Controllers/Dashboard/productsController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GeneralRequest;
+use App\Http\Requests\MainCategoryRequest;
+use App\Http\Requests\ProductImagesRequest;
+use App\Http\Requests\ProductPriceRequest;
+use App\Http\Requests\ProductStockRequest;
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Image;
 use App\Models\Product;
 use App\Models\Tag;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class productsController extends Controller
 {
     public function index(){
+
+        $products = Product::select('id','slug','price', 'created_at')->paginate(PAGINATION_COUNT);
+        return view('dashboard.products.general.index', compact('products'));
 
     }
 
@@ -29,9 +38,10 @@ class productsController extends Controller
 
 
     public function store(GeneralRequest $request){
+
         DB::beginTransaction();
 
-        //validation
+
 
         if (!$request->has('is_active'))
             $request->request->add(['is_active' => 0]);
@@ -58,5 +68,81 @@ class productsController extends Controller
         DB::commit();
         return redirect()->route('admin.products')->with(['success' => 'تم ألاضافة بنجاح']);
 
+
     }
+
+    public function getPrice($product_id){
+
+        return view('dashboard.products.prices.create') -> with('id',$product_id) ;
+    }
+
+    public function saveProductPrice(ProductPriceRequest $request){
+
+
+
+        try{
+
+            Product::whereId($request -> product_id) -> update($request -> only(['price','special_price','special_price_type','special_price_start','special_price_end']));
+
+            return redirect()->route('admin.products')->with(['success' => 'تم التحديث بنجاح']);
+        }catch(\Exception $ex){
+
+        }
+    }
+
+
+
+    public function getStock($product_id){
+
+        return view('dashboard.products.stock.create') -> with('id',$product_id) ;
+    }
+
+    public function saveProductStock (ProductStockRequest $request){
+
+
+            Product::whereId($request -> product_id) -> update($request -> except(['_token','product_id']));
+
+            return redirect()->route('admin.products')->with(['success' => 'تم التحديث بنجاح']);
+
+    }
+
+    public function addImages($product_id){
+        return view('dashboard.products.images.create')-> with('id',$product_id) ;
+    }
+
+
+    public function saveProductImages(Request $request ){
+
+        $file = $request->file('dzfile');
+        $filename = uploadImage('products', $file);
+
+        return response()->json([
+            'name' => $filename,
+            'original_name' => $file->getClientOriginalName(),
+        ]);
+
+    }
+
+    public function saveProductImagesDB(ProductImagesRequest $request){
+
+        try {
+
+            if ($request->has('document') && count($request->document) > 0) {
+                foreach ($request->document as $image) {
+                    Image::create([
+                        'product_id' => $request->product_id,
+                        'photo' => $image,
+                    ]);
+                }
+            }
+
+            return redirect()->route('admin.products')->with(['success' => 'تم التحديث بنجاح']);
+
+        }catch(\Exception $ex){
+
+        }
+    }
+
+
+
 }
---
+++ app/Models/Option.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    use HasFactory;
+    use Translatable;
+
+
+    protected $with = ['translations'];
+
+
+    protected $translatedAttributes = ['name'];
+
+
+
+    protected $fillable = ['attribute_id', 'product_id','price'];
+
+
+    protected $hidden = ['translations'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function attribute(){
+        return $this -> belongsTo(Attribute::class,'attribute_id');
+    }
+}
